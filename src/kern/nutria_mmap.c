5717d654d4aadffd1bd76466e2004d22ef7b5a2e
Aaron Wald <aaronwald@gmail.com>
2018-12-07 07:29:43 -0500

    nvm

--- src/kern/nutria_mmap.c
+++ src/kern/nutria_mmap.c
@@ -9,16 +9,16 @@ SEC("kprobe/ksys_read")
 // do
 int bpf_prog1(struct pt_regs *ctx)
 {
-	unsigned long long size;
-	char fmt[] = "sys_read size %d %d\n";
-	u32 pid;
-	pid = bpf_get_current_pid_tgid();
-	bpf_probe_read(&size, sizeof(size), (void *)&PT_REGS_PARM3(ctx));
-
-	if (pid == 4750)
-		bpf_trace_printk(fmt, sizeof(fmt), size, pid);
-
-	return 0;
+  unsigned long long size;
+  char fmt[] = "sys_read size %d %d\n";
+  u32 pid;
+  pid = bpf_get_current_pid_tgid();
+  bpf_probe_read(&size, sizeof(size), (void *)&PT_REGS_PARM3(ctx));
+  
+    if (pid == 11681 || pid == 11682)
+      bpf_trace_printk(fmt, sizeof(fmt), size, pid);
+  
+  return 0;
 }
 
 char _license[] SEC("license") = "GPL";
